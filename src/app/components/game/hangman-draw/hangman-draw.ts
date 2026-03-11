c8e9a8ceaa5a31c7ff5aae112a3bab979a2be8dc
Mondher Adoudi <adoudi.mondher@ik.me>
2026-03-11 10:42:20 +0100

    feat: génération des composants et services via Angular CLI

---
+++ src/app/components/game/hangman-draw/hangman-draw.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hangman-draw',
+  imports: [],
+  templateUrl: './hangman-draw.html',
+  styleUrl: './hangman-draw.css',
+})
+export class HangmanDraw {
+
+}
